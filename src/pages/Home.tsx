8813194f9822796458c9c2f406067c707f95f81d
Tim <tk.tim.flanke@gmail.com>
2023-05-03 16:03:30 +0200

    install react router dom

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+// import { useState } from "react";
+import Logo from "/holder-1080.jpg";
+
+function Home() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <header>
+        <img src={Logo} alt="bootshaus club logo"></img>
+      </header>
+    </>
+  );
+}
+
+export default Home;
